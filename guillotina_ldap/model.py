e323b8a3268451ca94975d9818f84e2290d0da5b
Ramon Navarro Bosch <ramon.nb@gmail.com>
2021-01-15 09:43:34 +0100

    Allowing testing import

--- guillotina_ldap/model.py
+++ guillotina_ldap/model.py
@@ -5,6 +5,13 @@ from guillotina_ldap.interfaces import ILDAPUsers
 
 class LDAPGuillotinaUser(GuillotinaUser):
 
-    async def set_password(self, password):
+    async def set_password(self, password, oldpassword=None):
         util = get_utility(ILDAPUsers)
-        await util.set_password(self.id, password)
+        set_password = False
+        if oldpassword is not None and await util.validate_user(self.id, oldpassword):
+            set_password = True
+        elif oldpassword is None:
+            set_password = True
+
+        if set_password:
+            await util.set_password(self.id, password)
